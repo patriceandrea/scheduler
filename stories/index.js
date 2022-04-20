f34b6232ab2eaa244cb3fae5be7c53374c8e0dfd
patriceandrea <andrea_pavico98@yahoo.ca>
2022-04-20 19:44:41 -0400

    add Form component to Appointment and stories

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,7 @@ import Show from "components/Appointment/Show.js";
 import Confirm from "components/Appointment/Confirm.js";
 import Status from "components/Appointment/Status.js";
 import Error from "components/Appointment/Error.js";
+import Form from "components/Appointment/Form.js";
 
 //Button
 storiesOf("Button", module)
@@ -174,6 +175,18 @@ storiesOf("Appointment", module)
   .add("Error", () => <Error
     message="Could not delete appointment."
     onClose={action("onClose")}
+  />)
+  .add("Create", () => <Form
+    interviewers={interviewers}
+    onSave={action("onSave")}
+    onCancel={action("onCancel")}
+  />)
+  .add("Edit", () => <Form
+    student="Patrice"
+    interviewer={1}
+    interviewers={interviewers}
+    onSave={action("onSave")}
+    onCancel={action("onCancel")}
   />)
   ;
 
