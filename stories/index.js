455485dc2a015ad43217bf87d7e8f3386022463a
patriceandrea <andrea_pavico98@yahoo.ca>
2022-04-20 17:13:46 -0400

    add Show component to Appointment

--- stories/index.js
+++ stories/index.js
@@ -10,7 +10,8 @@ import InterviewerListItem from "components/InterviewerListItem"
 import InterviewerList from "components/InterviewerList"
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header.js";
-import Empty from "components/Appointment/Empty";
+import Empty from "components/Appointment/Empty.js";
+import Show from "components/Appointment/Show.js";
 
 //Button
 storiesOf("Button", module)
@@ -154,5 +155,11 @@ storiesOf("Appointment", module)
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm" />)
-  .add("Empty", () => <Empty onAdd={action("onAdd")} />);
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+  .add("Show", () => <Show
+    student="Lydia Miller-Jones"
+    interviewer={interviewer}
+    onEdit={action("onEdit")}
+    onDelete={action("onDelete")}
+  />);
 
