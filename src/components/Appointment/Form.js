fb46d2bd64c677f93b2b5b3f697577f2a9983172
patriceandrea <andrea_pavico98@yahoo.ca>
2022-04-26 15:51:48 -0400

    add testing and error handlers for the Form component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,7 +6,7 @@ import { useState } from "react";
 function Form(props) {
   const [student, setStudent] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
-
+  const [error, setError] = useState("");
 
   const reset = () => {
     setStudent("")
@@ -22,6 +22,18 @@ function Form(props) {
     props.onSave(student, interviewer);
   };
 
+  function validate() {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    if (interviewer === null) {
+      setError("Please select an interviewer");
+      return;
+    }
+    props.onSave(student, interviewer);
+  }
+
 
   return (
     <main className="appointment__card appointment__card--create">
@@ -34,7 +46,9 @@ function Form(props) {
             placeholder="Enter Student Name"
             value={student}
             onChange={(event) => setStudent(event.target.value)}
+            data-testid="student-name-input"
           />
+          <section className="appointment__validation">{error}</section>
         </form>
         <InterviewerList
           interviewers={props.interviewers}
@@ -46,7 +60,7 @@ function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={save}>Save</Button>
+          <Button confirm onSubmit={save} onClick={validate}>Save</Button>
         </section>
       </section>
     </main>
