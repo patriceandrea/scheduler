9eeef42d7130076a6658f67527ee4827e57f5657
patriceandrea <andrea_pavico98@yahoo.ca>
2022-04-27 15:52:40 -0400

    add navigation test in cypress

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,5 +1,17 @@
 describe("Navigation", () => {
-  it("should visit root", () => {
+  beforeEach(() => {
     cy.visit("/");
+  })
+
+  it("should visit root", () => {
+  });
+
+  it("should navigate to Tuesday", () => {
+
+    cy.contains("li", "Tuesday")
+      .click()
+      // .should("have.css", "background-color", "rgb(242, 242, 242)");
+      .should("have.class", "day-list__item--selected")
   });
 });
+
